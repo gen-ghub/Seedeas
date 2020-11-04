5c3797c371048335c0dc3fa932ec6404d207597e
gen-ghub <renbenzhuang234@gmail.com>
2020-11-04 15:47:33 +0900

    マイページ編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"seeds#index"
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
+  get '/mypage' => 'users#mypage'
 end
